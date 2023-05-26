9d64ce3b21bd735e9bcc6272167039c4a5413811
srikar kudurmalla <91621052+srikar298c@users.noreply.github.com>
2023-05-26 22:49:34 +0530

    Created Componets

---
+++ Components/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+    return (
+        <>
+        <div>
+            <ul>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Resume & CV</li>
+                <li>Contact</li>
+
+            </ul>
+        </div>
+        </>
+    );
+}
